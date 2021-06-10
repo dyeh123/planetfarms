8e1325620b26ec7d509cf0fc6b5c0392717f92bf
Sujal Lama <lsujal41@gmail.com>
2021-06-10 19:03:16 +0545

    userinfo-responsive

--- src/Screens/cabinetUserInfo/CabinetUserInfo.jsx
+++ src/Screens/cabinetUserInfo/CabinetUserInfo.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './cabinet-user-info.css'
 import DashboardLayout from "../../Layout/DashboardLayout/DashboardLayout";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import BackButton from '../../Components/BackButton/BackButton';
+import useSizeFinder from '../../utils/SizeFinder';
+import Filter from '../../Components/Filter/Filter';
 
+const data = [{
+  label: 'Payment Information',
+  link: '/userinfo'
+}, {
+  label: 'Course reports',
+  link: '/userinfo/reports'
+}]
 
 function App() {
   return (
@@ -15,6 +24,8 @@ function App() {
 export default App;
 
 function CabinetUserInfo() {
+  const {pathname} = useLocation();
+  const windowWidth = useSizeFinder();
   return (
     <div className="payment-container">
       <div className="payment-flex-col-4">
@@ -22,28 +33,27 @@ function CabinetUserInfo() {
         {/* user details form comes here */}
         <div className="payment-card-container border-1px-onyx">
           <div className="payment-info">
-            <div className="payment-info-card border-0-5px-white">
-              <div className="payment-information ibmplexsans-semi-bold-shark-16px">Payment information</div>
-            </div>
-            <div className="payment-info-card-2 border-0-5px-white">
-              <div className="course-reports">Course reports</div>
-            </div>
+            {windowWidth > 600 
+            ? <ul className='library-list-container'>
+              {data.map((menu) => (
+                <li>
+                  <Link className={`nav-link ${(pathname === menu.link) ? 'library-list-item active' : 'library-list-item'}`} to={menu.link}>{menu.label}</Link>
+                </li>
+              ))}
+            </ul>
+            : <div className="payment-filter-container"> <Filter data={data} noImage="noImage" newFilter={true} />  </div>
+          }
           </div>
           <p className="payment-text ibmplexsans-regular-normal-quarter-spanish-white-16px">
             You can connect these payment services:
           </p>
-          <div className="payment-card-container-item">
-            <img src="/img/stripe-card.svg" />
-            <img src="/img/paypal-card.svg" />
-           <img src="/img/coinbase-card.svg" />
+          <div className="payment-option-container user-payment">
+              <button><img src="/img/stripe.svg" alt="stripe payment" /></button>
+              <button><img src="/img/paypal.svg" alt="paypal payment" /></button>
+              <button><img src="/img/Coinbase.svg" alt="coinbase payment" /></button>
           </div>
-          <div className="payment-card-container-item">
-            <div className="payment-overlap-group">
-              <div className="boderline"></div>
-              <div className="or-container">
-                <div className="or">OR</div>
-              </div>
-            </div>
+          <div className="or-icon-container">
+          <img className="or-icon user-info-or-icon" src="/img/or.svg" alt="or icon"/>
           </div>
           <div className="add-card border-0-5px-quarter-spanish-white">
             {/* <div className="frame-1933"></div> */}
